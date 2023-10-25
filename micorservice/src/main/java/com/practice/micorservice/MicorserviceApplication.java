d460b7a7c6a72b8d91f19b7aec1543cac5285aac
Abhishek Kumar Pandey <60402251+akp-pandey@users.noreply.github.com>
2023-10-25 21:36:03 +0530

    initial commit

---
+++ micorservice/src/main/java/com/practice/micorservice/MicorserviceApplication.java
@@ -0,0 +1,13 @@
+package com.practice.micorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicorserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicorserviceApplication.class, args);
+	}
+
+}
---
+++ micorservice/src/test/java/com/practice/micorservice/MicorserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.micorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicorserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
